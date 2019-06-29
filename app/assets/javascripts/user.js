3d761623e91d5af4e5f84fc2e0ec87fafebca50f
KawakamiYuka0424 <siratama00@gmail.com>
2019-06-29 21:04:22 +0900

    イベントを非同期通信で行えるようにする。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,13 @@
 $(function() {
   $('.chat-group-form__input').on('keyup', function() {
-    
+
+    var input = $('#user-search-field').val();
+    $.ajax({
+      url: '/users',
+      type: "GET",
+      data: { keyword: input },
+      dataType: 'json',
+    })
 
   })
 })
