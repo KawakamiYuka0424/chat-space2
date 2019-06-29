d47a7b868944211a618898f936fded9c7408eb69
KawakamiYuka0424 <siratama00@gmail.com>
2019-06-29 21:14:50 +0900

    userリストのHTML追加。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,31 @@
-$(function() {
-  $('.chat-group-form__input').on('keyup', function() {
+$(function(){
 
-    var input = $('#user-search-field').val();
-    $.ajax({
-      url: '/users',
-      type: "GET",
-      data: { keyword: input },
-      dataType: 'json',
+  var user_list = $("#user_search_result");
+
+  function appendUsers(user) {
+    var html =`<div class="chat-group-user clearfix js-chat-user">
+                <p class="chat-group-user__name">
+                ${user.name}
+                </p>
+                <a class="user_search_add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                </a>
+                </div>`
+
+    user_list.append(html);
+  }
+
+
+  $(function() {
+    $('.chat-group-form__input').on('keyup', function() {
+
+      var input = $('').val();
+      $.ajax({
+        url: '/users',
+        type: "GET",
+        data: { keyword: input },
+        dataType: 'json',
+      })
+
     })
-
   })
-})
+});
